df441eb042a955479add5cf1de2839c6c842e39c
Fedot1k <frasssepkin@gmail.com>
2024-12-05 17:37:06 +0300

    kobe dream big

--- patterns/16-recur.py
+++ patterns/16-recur.py
@@ -1,18 +1,11 @@
+import sys
+
+sys.setrecursionlimit(1000000000)
+
 def f(n):
-    if n > 25:
-        return (n * n) + (2 * n) + 1
-    elif n <= 25 and n % 2==0:
-        return (2 * f(n + 1)) + f(n + 3)
-    elif n <= 25 and n % 2 != 0:
-        return f(n + 2) + (3 * f(n + 5))
+    if n <= 1:
+        return 3
+    else:
+        return f(n-1) + 2 * f(n-2) - 5
 
-
-res = 0
-
-for i in range(1, 1001):
-    p = f(i)
-    s = [int(x) for x in str(p)]
-    if 0 not in s:
-        res += 1
-
-print(res)
+print(f(100))
