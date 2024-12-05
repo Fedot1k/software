b74c6a377c3df42810c6c5b9510a98c184c25d62
Fedot1k <frasssepkin@gmail.com>
2024-12-05 17:56:20 +0300

    kobe dream big

--- patterns/8-repeat.py
+++ patterns/8-repeat.py
@@ -2,10 +2,9 @@ from itertools import *
 
 cnt = 0
 
-for x in product('01234567', repeat = 6):
+for x in product('ГЕНОСТХЮ', repeat = 4):
+    cnt += 1
     s = ''.join(x)
-    b = s.replace('1', '*').replace('3', '*').replace('5', '*').replace('7', '*').replace('0', '#').replace('2', '#').replace('4', '#').replace('6', '#')
-    if s[0] != '0' and len(set(s)) == len(s) and '**' not in b and '##' not in b:
-        cnt += 1
+    if (s[0] != 'Н') and (s.count('О') >= 2) and ('С' not in s) and (cnt % 2!= 0):
+        print(cnt)
 
-print(cnt)
