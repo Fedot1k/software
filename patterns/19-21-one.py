423139300df405e1b0fdc4ce35cd2d1cd6d4c2e8
Fedot1k <frasssepkin@gmail.com>
2024-12-26 18:06:41 +0300

    kobe dream big

--- patterns/19-21-one.py
+++ patterns/19-21-one.py
@@ -2,17 +2,17 @@ from functools import lru_cache
 import math
 
 def moves(s):
-    return s + 1, s * 2  # возможные действия
+    return s + 1, s * 3 # возможные действия
 
 @lru_cache(None)
 
 def game(s):
-    if s >= 50: return 'w' # число необходимое для победы
+    if s >= 56: return 'w' # число необходимое для победы
     if any(game(m) == 'w' for m in moves(s)): return 'p1'
     if all(game(m) == 'p1' for m in moves(s)): return 'b1' # <all> для нормальной игры, <any> для для плохой игры
     if any(game(m) == 'b1' for m in moves(s)): return 'p2'
     if all(game(m) == 'p1' or game(m) == 'p2' for m in moves(s)): return 'b2'
 
-for s in range(1, 50): # количество камней в куче
+for s in range(1, 56): # количество камней в куче
     if game(s) == 'b2': # b - Ваня, p - Петя, 1 - первый ход, 2 - второй ход
         print(s, game(s))
--- patterns/19-21-two.py
+++ patterns/19-21-two.py
@@ -3,21 +3,24 @@ import math
 
 def moves(s):
     a, b = s
-    return (a+2, b), (a*2, b), (a, b+2), (a, b*2) # возможные действия
+
+    if a < b:
+        return (a+1, b), (a+3, b) # возможные действия
+    if b < a:
+        return (a, b+1), (a, b+3) # возможные действия
 
 @lru_cache(None)
 
 def game(s):
     a, b = s
 
-    if a + b >= 100: return 'w' # число необходимое для победы
+    if a == b: return 'w' # число необходимое для победы
     if any(game(m) == 'w' for m in moves(s)): return 'p1'
-    if any(game(m) == 'p1' for m in moves(s)): return 'b1' # <all> для нормальной игры, <any> для для плохой игры
+    if all(game(m) == 'p1' for m in moves(s)): return 'b1' # <all> для нормальной игры, <any> для для плохой игры
     if any(game(m) == 'b1' for m in moves(s)): return 'p2'
     if all(game(m) == 'p1' or game(m) == 'p2' for m in moves(s)): return 'b2'
 
-
-for s_ in range(1, 81):
-    s = 9, s_ # две кучи камней, s - камней в первой, s_ - камней во второй
-    if game(s) == 'b1': # b - Ваня, p - Петя, 1 - первый ход, 2 - второй ход
+for s_ in range(1, 24):
+    s = 13, s_ # две кучи камней, s - камней в первой, s_ - камней во второй
+    if game(s) == 'b2': # b - Ваня, p - Петя, 1 - первый ход, 2 - второй ход
         print(s)
