742faab09df0d58c30feb461a62a416d9397fc77
Fedot1k <frasssepkin@gmail.com>
2025-01-07 21:00:56 +0300

    kobe dream big

--- patterns/19-21-two.py
+++ patterns/19-21-two.py
@@ -3,24 +3,19 @@ import math
 
 def moves(s):
     a, b = s
-
-    if a < b:
-        return (a+1, b), (a+3, b) # возможные действия
-    if b < a:
-        return (a, b+1), (a, b+3) # возможные действия
+    return (a+1, b), (a, b+1), (a*2, b), (a, b*2) # возможные действия
 
 @lru_cache(None)
 
 def game(s):
-    a, b = s
-
-    if a == b: return 'w' # число необходимое для победы
+    if sum(s) >= 77: return 'w' # число необходимое для победы
     if any(game(m) == 'w' for m in moves(s)): return 'p1'
     if all(game(m) == 'p1' for m in moves(s)): return 'b1' # <all> для нормальной игры, <any> для для плохой игры
     if any(game(m) == 'b1' for m in moves(s)): return 'p2'
     if all(game(m) == 'p1' or game(m) == 'p2' for m in moves(s)): return 'b2'
 
-for s_ in range(1, 24):
+
+for s_ in range(1, 77):
     s = 13, s_ # две кучи камней, s - камней в первой, s_ - камней во второй
     if game(s) == 'b2': # b - Ваня, p - Петя, 1 - первый ход, 2 - второй ход
         print(s)
--- patterns/23-command.py
+++ patterns/23-command.py
@@ -1,6 +1,11 @@
-def f(s, k):
-    if s == k: return 1
-    if s > k: return 0
-    return f(s + 1, k) + f(s * 2, k) # возможные действия
+def f(s, x):
+    if x == 1001:
+        res.append(s)
+        return 1
 
-print(f(1, 24)) # s - начало, k - конец
+    return f(s + 2, x + 1) + f(s + 4, x + 1) + f(s + 5, x + 1) # возможные действия
+
+print(f(31, 0))
+
+print(res)
+
