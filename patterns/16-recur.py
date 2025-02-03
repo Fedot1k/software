da144a02110c6ac2fa0c978b6b894c19b6454517
Fedot1k <frasssepkin@gmail.com>
2025-02-03 09:04:47 +0300

    kobe dream big

--- patterns/16-recur.py
+++ patterns/16-recur.py
@@ -2,17 +2,13 @@ from functools import lru_cache
 
 @lru_cache(None)
 
+def f(n):
+    if n == 1:
+        return 1
+    if n > 0:
+        return f(n-1) * n
 
-def f(n, m):
-    if m > n:
-        return 0
-    elif m <= n and n%m==0:
-        return 1 + f(n, m+1)
-    elif m <= n and n%m!=0:
-        return f(n, m+1)
-
-
-print(f(107864, 3))
+print(f(15))
 
 
 
