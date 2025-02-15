9ec0c31ffa1c7fcaad7573fd90b8ab719e1a07cc
Fedot1k <frasssepkin@gmail.com>
2025-02-15 09:08:22 +0300

    kobe dream big

--- patterns/main.py
+++ patterns/main.py
@@ -1,13 +1,10 @@
-with open('task-7.txt', 'r') as file:
+with open('stuff/task.txt') as file:
     lines = file.readlines()
 
 big = [list(map(int, l.split())) for l in lines]
 
-for row in range(len(big)):
-    if len(set(big[row])) == 5:
-        repet = [x for x in big[row] if big[row].count(x) > 1]
-        if max(repet) != max(big[row]) and len(repet) == 4:
-            print(row)
-            break
-
-print(sum(big[786]))
+for row in big:
+    repet = [x for x in row if row.count(x) == 2]
+    if len(repet) == 4 and max(row) not in repet:
+        print(row)
+        break
