7d013c9e9b45a059356c7f62198346e31d15ccfb
Fedot1k <frasssepkin@gmail.com>
2025-02-20 08:53:13 +0300

    kobe dream big

---
+++ patterns/5-math.py
@@ -0,0 +1,11 @@
+for n in range(1, 1000):
+    st = bin(n)[2:]
+    
+    if st.count('1') % 2 == 0:
+        x = '10' + st[2:] + '0'
+    else:
+        x = '11' + st[2:] + '1'
+
+    if int(x, 2) > 40:
+        print(n)
+        break
--- patterns/main.py
+++ patterns/main.py
@@ -1,10 +1,6 @@
-with open('stuff/task.txt') as file:
-    lines = file.readlines()
-
-big = [list(map(int, l.split())) for l in lines]
-
-for row in big:
-    repet = [x for x in row if row.count(x) == 2]
-    if len(repet) == 4 and max(row) not in repet:
-        print(row)
-        break
+for x in range(111):
+    f1 = x * 111**3 + 3 * 111**2 + 2 * 111 + 1
+    f2 = 1 * 211**3 + 7 * 211**2 + x * 211 + 4
+    f = f1 + f2
+    if f%111==0:
+        print(f/111)
