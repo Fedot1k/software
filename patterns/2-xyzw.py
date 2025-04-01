00605de63710d6663c1ce7d0f63e94e75d09b15b
Fedot1k <frasssepkin@gmail.com>
2025-04-01 19:09:27 +0300

    kobe dream big

--- patterns/2-xyzw.py
+++ patterns/2-xyzw.py
@@ -1,9 +1,9 @@
 print('x y z w')
 
-for x in (0, 1):
-    for y in (0, 1):
-        for z in (0, 1):
-            for w in (0, 1):
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
                 f = z <= (x == (w <= y))  # выражение из задания
                 if f == 0:
                     print(x, y, z, w, '=', int(f))
--- stuff/main.py
+++
@@ -1,8 +0,0 @@
-def f(s, k, last):
-    if s == k:
-        return 1
-    if s > k+1 or s % 10 == 0:
-        return 0
-    return (0 if last else f(s-1, k, True)) + f(s+2, k, False) + f(s*3, k, False)
-
-print(f(5, 32, False) * f(32, 62, False))
