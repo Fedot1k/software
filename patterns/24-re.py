5226590123712ac92f6c5e16b3c89b721bff7d82
Fedot1k <frasssepkin@gmail.com>
2025-04-05 11:03:33 +0300

    kobe dream big

--- patterns/24-re.py
+++ patterns/24-re.py
@@ -2,8 +2,6 @@ from re import findall
 
 s = open('stuff/24-express.txt').readline()
 
-check = findall(r'(?:0|[1-9][0-9]*)(?:[+*](?:0|[1-9][0-9]*))*', s)  # корректные выражения
+res = findall(r'(?:0|[1-9][0-9]*)(?:[+*](?:0|[1-9][0-9]*))*', s)  # корректные выражения
 
-res = [len(x) for x in check]
-
-print(max(res))
+print(max([len(x) for x in res]))
